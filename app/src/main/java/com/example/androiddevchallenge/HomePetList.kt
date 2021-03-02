d059d47130bf86c15a1baa65a2283a31a08e0703
yaoyu <yugait@163.com>
2021-03-03 01:55:54 +0800

    修改错误

--- app/src/main/java/com/example/androiddevchallenge/HomePetList.kt
+++ app/src/main/java/com/example/androiddevchallenge/HomePetList.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
